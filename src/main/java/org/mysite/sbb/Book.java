1584e232e3dff2fe0be0e2ad6b94385e6cdb602a
ohsang14 <ast6108@naver.com>
2024-08-31 02:35:42 +0900

    2. 스프링 부트의 기본

--- src/main/java/org/mysite/sbb/Book.java
+++ src/main/java/org/mysite/sbb/Book.java
@@ -1,4 +1,23 @@
 package org.mysite.sbb;
 
 public class Book {
+    private String title;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    private String author;
 }
