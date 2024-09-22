b04dd334e13e92adac2c4a54030ea103590070c8
ohsang14 <ast6108@naver.com>
2024-09-22 17:26:23 +0900

    9월 22일

--- src/test/java/org/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/org/mysite/sbb/SbbApplicationTests.java
@@ -18,17 +18,8 @@ class SbbApplicationTests {
 
     @Test
     void testJpa() {
-       Question q1 = new Question();
-       q1.setSubject("sbb가 무엇인가요?");
-       q1.setContect("sbb에 대해서 알고 싶습니다.");
-       q1.setCreateDate(LocalDateTime.now());
-       this.questionRepository.save(q1);
-
-        Question q2 = new Question();
-        q2.setSubject("스프링 부트 모델 질문입니다");
-        q2.setContect("id는 자동으로 생성되나요?");
-        q2.setCreateDate(LocalDateTime.now());
-        this.questionRepository.save(q2);
+        Question q = this.questionRepository.findBySubjectAndContect("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+        assertEquals(1,q.getId());
     }
 
 }
