82b4a6d30b36dbe6d238969f3be7f762617d8e91
ohsang14 <ast6108@naver.com>
2024-09-23 10:53:39 +0900

    9월 23일

--- src/main/java/org/mysite/sbb/QuestionRepository.java
+++ src/main/java/org/mysite/sbb/QuestionRepository.java
@@ -2,7 +2,10 @@ package org.mysite.sbb;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
     Question findBySubject(String subject);
     Question findBySubjectAndContect(String subject, String contect);
+    List<Question> findBySubjectLike(String subject);
 }
--- src/test/java/org/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/org/mysite/sbb/SbbApplicationTests.java
@@ -7,19 +7,37 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.Queue;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class SbbApplicationTests {
 
-    @Autowired
+    // Question 테이블에 CRUD 작업을 지원하는 객체를
+    // DI(Dependency Injection) 받는다.
+    @Autowired // 자동으로 객체를 만들어줌 (new 이런식 필요 없)
     private QuestionRepository questionRepository;
 
     @Test
-    void testJpa() {
-        Question q = this.questionRepository.findBySubjectAndContect("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
-        assertEquals(1,q.getId());
+    void testJpa01() {
+        Question q1 = new Question();
+        q1.setId(1);
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContect("sbb에 대해서 알고 싶습니다.");
+        q1.setCreateDate(LocalDateTime.now());
+
+        this.questionRepository.save(q1);
+
+
+        Question q2 = new Question();
+        q2.setId(2);
+        q2.setSubject("스프링 부트 모델 질문 입니다.");
+        q2.setContect("id는 자동으로 생성되나요?");
+        q2.setCreateDate(LocalDateTime.now());
+
+        this.questionRepository.save(q2);
     }
+
 
 }
