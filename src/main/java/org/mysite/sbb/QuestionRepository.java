45fe39496549074dac6a9541181525f489528df6
ohsang14 <ast6108@naver.com>
2024-09-30 10:51:36 +0900

    9월 30일

--- src/main/java/org/mysite/sbb/QuestionRepository.java
+++ src/main/java/org/mysite/sbb/QuestionRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    // List<Question> findBySubject(String subject);
     Question findBySubject(String subject);
     Question findBySubjectAndContect(String subject, String contect);
     List<Question> findBySubjectLike(String subject);
--- src/test/java/org/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/org/mysite/sbb/SbbApplicationTests.java
@@ -19,6 +19,11 @@ class SbbApplicationTests {
     @Autowired // 자동으로 객체를 만들어줌 (new 이런식 필요 없)
     private QuestionRepository questionRepository;
 
+    // Answer 테이블에 CRUD 작업을 지원하는 객체를
+    // DI (Dependency Injection) 받는다.
+    @Autowired
+    private AnswerRepository answerRepository;
+
     @Test
     void testJpa01() {
         Question q1 = new Question();
@@ -94,20 +99,108 @@ class SbbApplicationTests {
 
     @Test
     void testJpa07() {
+        /*
+        id가 1번인 레코드의 제목(Subject)을 '수정된 제목'으로 변경
+        SQL : update question set subject = '수정된 제목' where id = 1;
+        Repository 메서드를 사용해서 위의 SQL문이 내부적으로 수행되도록 하려면
+        1. 리포지토리의 findById 메서드를 해당 엔티티를 찾고
+        2. 그 엔티티의 subject를 수정한 후
+        3. 리포지토리의 save 메서드로 그 엔티티를 저장.
+         */
         Optional<Question> oq = this.questionRepository.findById(1);
-        assertTrue(oq.isPresent());
+        assertTrue(oq.isPresent()); // isPresent - 물어보는거 , Present - 존재하다.
         Question q = oq.get();
         q.setSubject("수정된 제목");
         this.questionRepository.save(q);
     }
+
     @Test
-    void testJpa08(){
+    void testJpa08() {
+        /*
+        현재 question 테이블에 레코드가 2개 있는 것을 확인.
+        id가 1번인 레코드를 삭제하고
+        삭제 후에 question 테이블에 레코드가 1개인 것을 확인.
+         */
+
+        /*
+        question 테이블에 레코드 수를 확인하는 SQL문
+        => select count(*)  from question;
+        Repository 메서드로는 count()를 사용하면 됨.
+         */
+
+        /*
+        question 테이블에서 id가 1인 레코드 삭제하는 SQL문
+        => delete from question where id = 1;
+        1. Repository를 이용해서 레코드를 삭제하기 위해서는
+        먼저 findById() 메서드 등을 통해 삭제하고자
+        하는 entiry 객체를 찾고 :
+            Optional<Question> oq = this.questionRepository.findById(1);
+        2. 그 객체를 Repository의 delete 메서드의 인자로 전달해 호출하면된다.
+        Question q = oq.get();
+        this.questionRepository.delete(q);
+         */
         assertEquals(2, this.questionRepository.count());
         Optional<Question> oq = this.questionRepository.findById(1);
         assertTrue(oq.isPresent());
         Question q = oq.get();
         this.questionRepository.delete(q);
-        assertEquals(1,this.questionRepository.count());
+        assertEquals(1, this.questionRepository.count());
+
+
     }
 
+    @Test
+    void testJpa09() {
+        Optional<Question> oq = this.questionRepository.findById(2);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+
+        Answer a = new Answer();
+        a.setContent("네 자동으로 생성됩니다.");
+        a.setQuestion(q);
+        a.setCreateDate(LocalDateTime.now());
+        this.answerRepository.save(a);
+    }
+
+    @Test
+    void testJpa10() {
+        /*
+            2번 질문에 대한 답변 레코드를 저장하자.
+            답변 레코드는 Answer 테이블에 저장되고,
+            SQL: insert into answer(content, createdate, question_id)
+                values('~답변', '2024/09/30', 2);
+                주의: question_id의 값은 반드시 question 테이블에 존재하는 레코드의 id 칼럼 값이어야 한다.
+                그렇지 않으면 참조 무결성 오류가 발생해서 레코드가 삽입되지 않는다. referentibl integrity violation.
+            answer 테이블에 저장하기 위해서는
+            AnswerRepository를 사용해야 한다.
+            1. 질문 엔티티 객체를 찾는다.
+                Optional<Question> oq = this.questionRepository.findById(2);
+            2. 답변 엔티티 객체를 생성한다.
+                Answer answer = new Answer();
+            3. 답변 엔티티 객체의 프로퍼티를 설정한다.
+                answer.setContent("~~~~답변입니다.");ß
+                answer.setCreateData(LocalDateTime.now());
+                // 이답변에 연결된 질문 객체 설정
+                answer.seQuestion(oq.get());
+            4. AnswerRepository의 save() 메서드를 호출해 DB에 저장
+                this.answerRepository.save(answer);
+         */
+        Optional<Answer> oa = this.answerRepository.findById(1);
+        assertTrue(oa.isPresent());
+        Answer a = oa.get();
+        assertEquals(2, a.getQuestion().getId());
+    }
+
+    // @Test
+    void testJpa11() {
+        Optional<Question> oq = this.questionRepository.findById(2);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+
+        List<Answer> answerList = q.getAnswerList();
+
+        assertEquals(1, answerList.size());
+        assertEquals("네 자동으로 생성됩니다.", answerList.get(0).getContent());
+
+    }
 }
