61d5eb955fe8f125d33ec9b4600bf64c6247c129
Ohsang14 <ast6108@naver.com>
2024-11-04 10:44:11 +0900

    11월 4일 spring boot

--- src/main/java/org/mysite/sbb/question/QuestionController.java
+++ src/main/java/org/mysite/sbb/question/QuestionController.java
@@ -1,8 +1,11 @@
 package org.mysite.sbb.question;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.mysite.sbb.answer.AnswerForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,25 +21,28 @@ public class QuestionController {
     public String list(Model model) {
         List<Question> questionList = this.questionService.getList();
         model.addAttribute("questionList", questionList);
-        return "question_list";
+        return "question_list"; // 질문 목록 페이지를 렌더링
     }
 
     @GetMapping(value = "/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Integer id) {
-        // id에 해당하는 Question 객체를 DB에서 인출하고
+    public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
         Question question = this.questionService.getQuestion(id);
-        // 그 객체를 model에 넣어준다.
         model.addAttribute("question", question);
-        return "question_detail";
+        return "question_detail"; // 질문 상세 페이지를 렌더링
     }
+
     @GetMapping("/create")
-    public String questionCreate(){
-        return "question_form";
+    public String questionCreate(Model model) {
+        model.addAttribute("questionForm", new QuestionForm()); // 폼 객체를 모델에 추가
+        return "question_form"; // 질문 등록 폼을 보여줌
     }
+
     @PostMapping("/create")
-    public String questionCreate(@RequestParam(value="subject")String subject,
-                                 @RequestParam(value="content")String content){
-        this.questionService.create(subject,content);
-        return "redirect:/question/list";
+    public String questionCreate(@Valid @ModelAttribute("questionForm") QuestionForm questionForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "question_form"; // 오류가 있을 경우 다시 폼으로
+        }
+        this.questionService.create(questionForm.getSubject(), questionForm.getContent());
+        return "redirect:/question/list"; // 성공적으로 저장 후 목록으로 리다이렉트
     }
 }
